47cf1a6de28d4650b040d37b0cbe61e48c39057e
Katmitsu <ikaterynh@gmail.com>
2025-08-03 17:45:05 +0100

    Added more context messages to AI

--- src/index.js
+++ src/index.js
@@ -16,8 +16,12 @@ function generatePoem(event) {
   let apiKey = "224f23t3b08b6cab3df63781o3c48aaf";
   let prompt = `Write a poem about romance ${instructionsInput.value}`;
   let context =
-    "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line short poem based on the user's input. Just use tags like <p> and <br> to format the poem. Sign the poem with `SheCodes AI` at the end of the poem, inside a <strong> element. Do not use any other tags or HTML elements. Also use cute emojis to make the poem more engaging. Do not use any other text or explanation, just the poem itself.";
+    "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line short poem based on the user's input. Just use tags like <p> and <br> to format the poem. Sign the poem with `SheCodes AI` at the end of the poem, inside a <strong> element. Do not use any other tags or HTML elements. Also use cute emojis to make the poem more engaging. Do not use any other text or explanation, just the poem itself. Also you can do it in any language, but make sure to use the language of the user's input. If the user input is in English, write the poem in English, if it is in French, write it in French, and so on in any language. So make sure that you recognize the language of the user's input and write the poem in that language. Recognize which language does that world belongs to and write the poem in that language. ";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `Generating a romantic poem about ${instructionsInput.value}...`;
 
   console.log("Generating poem");
   console.log("#user-instructions", instructionsInput.value);
